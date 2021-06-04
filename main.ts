231bcc25861f9ea5cfd751c506a8577702b798ad
Ada <70569108+adax0107@users.noreply.github.com>
2021-06-04 08:14:12 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-
+let farve = 0
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    farve = randint(1, 8)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    if (farve == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else if (farve == 2) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else if (farve == 3) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else if (farve == 4) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else if (farve == 5) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else if (farve == 6) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else if (farve == 7) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+})
